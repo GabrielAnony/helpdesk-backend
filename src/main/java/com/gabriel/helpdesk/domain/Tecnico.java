75323f03ed53cd0d61eaa86be0a3dc8b8750f55c
GabrielAnony <gabriel18lb@outlook.com>
2023-03-17 16:18:48 -0300

    metodo create tecnico

--- src/main/java/com/gabriel/helpdesk/domain/Tecnico.java
+++ src/main/java/com/gabriel/helpdesk/domain/Tecnico.java
@@ -2,11 +2,13 @@ package com.gabriel.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gabriel.helpdesk.domain.dto.TecnicoDTO;
 import com.gabriel.helpdesk.domain.enums.Perfil;
 
 import lombok.Getter;
@@ -31,6 +33,18 @@ public class Tecnico extends Pessoa {
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		setPerfil(Perfil.TECNICO);
+	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+		setPerfil(Perfil.CLIENTE);
 	}
 
 }
--- src/main/java/com/gabriel/helpdesk/services/TecnicoService.java
+++ src/main/java/com/gabriel/helpdesk/services/TecnicoService.java
@@ -1,11 +1,13 @@
 package com.gabriel.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.helpdesk.domain.Tecnico;
+import com.gabriel.helpdesk.domain.dto.TecnicoDTO;
 import com.gabriel.helpdesk.exceptions.ObjectNotFoundException;
 import com.gabriel.helpdesk.repositories.TecnicoRepository;
 
@@ -18,6 +20,16 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {	
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: "+id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 
 

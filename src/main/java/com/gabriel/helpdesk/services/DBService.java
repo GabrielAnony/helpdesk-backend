6617f21da3259cdd9ffab8ec9a81a740e3519a27
unknown <gabriel.bezerra@d2ti.com.br>
2023-03-18 18:39:39 -0300

    curso angular

--- src/main/java/com/gabriel/helpdesk/services/DBService.java
+++ src/main/java/com/gabriel/helpdesk/services/DBService.java
@@ -27,7 +27,7 @@ public class DBService {
 
 	public void instanciaDB() {
 
-		Tecnico tec1 = new Tecnico(null, "Gabriel Lima", "024.999.252-30", "gabriel", encoder.encode("123"));
+		Tecnico tec1 = new Tecnico(null, "Gabriel Lima", "024.999.252-30", "gabriel@gmail.com", encoder.encode("123"));
 		tec1.addPerfil(Perfil.ADMIN);
 		Tecnico tec2 = new Tecnico(null, "Richard Stallman", "903.347.070-56", "stallman@mail.com", encoder.encode("123"));
 		Tecnico tec3 = new Tecnico(null, "Claude Elwood Shannon", "271.068.470-54", "shannon@mail.com", encoder.encode("123"));

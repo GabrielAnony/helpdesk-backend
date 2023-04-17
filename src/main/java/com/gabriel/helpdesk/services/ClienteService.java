3c97a05dd315e140b8174f648f128402175c7198
unknown <gabriel.bezerra@d2ti.com.br>
2023-04-17 16:16:38 -0300

    ajuste deploy

--- src/main/java/com/gabriel/helpdesk/services/ClienteService.java
+++ src/main/java/com/gabriel/helpdesk/services/ClienteService.java
@@ -59,7 +59,7 @@ public class ClienteService {
 	public void delete(Integer id) {
 		Cliente obj = findById(id);
 
-		if (obj.getChamados().size() > 0) {
+		if (!obj.getChamados().isEmpty()) {
 			throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
 		}
 
